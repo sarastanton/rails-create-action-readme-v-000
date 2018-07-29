11b25794df0dcac6b96e2cd6298b15aed892bb5c
Sara Stanton <sara.mpa13@gmail.com>
2018-07-29 07:23:45 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,9 +9,10 @@ class PostsController < ApplicationController
 
 	def new
 		@post = Post.new
-		post = Post.new
-		post.title = params[:title]
-		post.description = params[:description]
-		post.save
+		@post = Post.new
+		@post.title = params[:title]
+		@post.description = params[:description]
+		@post.save
+		redirect_to post_path(@post)
 	end
 end
